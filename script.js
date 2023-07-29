00a9827c47c116b0094c313bcdd9045fd1a01a61
Mayesha <104177936+GlitzyIsBored@users.noreply.github.com>
2023-07-29 10:42:37 +1000

    added fil
    add

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById('character').setAttribute('draggable',false);
+
+
+var character = 
+document.getElementById("character"); 
+var block = document.getElementById("block");
+function jump() {
+	if(character.classList != "animate") {
+	character.classList.add("animate"); 
+	}
+setTimeout(function(){
+	character.classList.remove("animate");
+}, 500);
+}
+var checkDead = setInterval(function(){
+	var characterTop =
+parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+var blockLeft =
+parseInt(window.getComputedStyle(block).getPropertyValue("left")); 
+if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+	block.style.animation = "none";
+	block.style.display = "none";
+	alert("lol rip");
+}
+ },10);
+	
+	
+/*	var blockLeft =
+	parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+	if(blockLeft<20 && blockleft>0 && characterTop>=130{
+		block.style.animation = "none";
+		block.style.display = "none";
+		alert("rip");
+	})
+},10); */
